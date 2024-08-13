f043335e329601e20ba7acf5b7f4832c4cbaa72c
Admin <Admin@Jyoti>
2024-08-13 11:54:04 +0530

    shivani

---
+++ SeleniumMavenProject/src/test/java/Demo/sample.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample {
+@Test
+public void demo()
+{
+	Reporter.log("Sample class executed",true);
+}
+}
